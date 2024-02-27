484770e2a10143bcb62ba35fe725d8fc1730214f
CarlosPaz64 <134614533+CarlosPaz64@users.noreply.github.com>
2024-02-26 19:49:37 -0600

    Pruebas

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ encabezadoSticky.id = "myHeader";
 // Agregar imagen de encabezado
 var img = document.createElement('img');
 
+document.write("Seleccionar idioma: ")
+
 img.src = 'images/f92d5f2f7d56636846861c458c0d0b6c.svg';
 
 document.body.appendChild(encabezadoSticky);
